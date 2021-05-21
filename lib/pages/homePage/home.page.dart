3ab3f3677dc4093252b41d822c4d23e1b75d0a5b
Mark Music <markmusic999@gmail.com>
2021-05-20 21:11:58 -0600

    ft[main.dart] => 🧊 Setup basic pages with routing

---
+++ lib/pages/homePage/home.page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static String id = "homePage_id";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
---
+++ lib/pages/moreInfoPage/moreInfo.page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MoreInfoPage extends StatelessWidget {
+  static String id = "moreInfoPage_id";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
