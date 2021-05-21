ed9ac65faa1738016158e0e86ff778b21ff06e52
Mark Music <markmusic999@gmail.com>
2021-05-20 23:19:16 -0600

    ft[core] => 🥺 Setup core infrastructure with providers

---
+++ lib/core/core.dart
@@ -0,0 +1,5 @@
+import 'package:cartoon_repository/state/state.dart';
+
+class Core {
+  State state = State();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:cartoon_repository/core/core.dart';
 import 'package:cartoon_repository/pages/homePage/home.page.dart';
 import 'package:cartoon_repository/pages/moreInfoPage/moreInfo.page.dart';
 import 'package:cartoon_repository/utils/constants.util.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(CartoonArchiveApp());
@@ -10,17 +12,22 @@ void main() {
 class CartoonArchiveApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: kScaffoldBackgroundColor,
-        textTheme: kTextTheme,
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => Core()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: kScaffoldBackgroundColor,
+          textTheme: kTextTheme,
+        ),
+        routes: {
+          HomePage.id: (context) => HomePage(),
+          MoreInfoPage.id: (context) => MoreInfoPage(),
+        },
+        initialRoute: HomePage.id,
       ),
-      routes: {
-        HomePage.id: (context) => HomePage(),
-        MoreInfoPage.id: (context) => MoreInfoPage(),
-      },
-      initialRoute: HomePage.id,
     );
   }
 }
